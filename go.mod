edd75f8ecd4612fe60e441fc6406198df25c18a0
Scott Cooper <sgcooper78@gmail.com>
2025-02-04 09:24:00 -0700

    intial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sgcooper78/go-calculator
+
+go 1.23.5
---
+++ prices/prices.go
@@ -0,0 +1,14 @@
+package prices
+
+type TaxIncludedPriceJob struct {
+	TaxRate           float64
+	InputPrices       []float64
+	TaxIncludedPrices map[string]float64
+}
+
+func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
+	return &TaxIncludedPriceJob{
+		TaxRate:     taxRate,
+		InputPrices: []float64{10, 20, 30},
+	}
+}
