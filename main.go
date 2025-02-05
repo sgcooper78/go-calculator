7d2ed4cbed0a835c69f0d979b07a1d35a0706f8b
Scott Cooper <sgcooper78@gmail.com>
2025-02-05 09:05:26 -0700

    split out packages

--- main.go
+++ main.go
@@ -1,20 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"github.com/sgcooper78/go-calculator/prices"
+)
 
 func main() {
-	prices := []float64{10, 20, 30}
 	taxRates := []float64{0, 0.7, 0.1, 0.15}
 
-	result := make(map[float64][]float64)
-
 	for _, taxRate := range taxRates {
-		var taxIncludedPrices []float64 = make([]float64, len(prices))
-		for priceIndex, price := range prices {
-			taxIncludedPrices[priceIndex] = price * (1 + taxRate)
-		}
-		result[taxRate] = taxIncludedPrices
+		priceJob := prices.NewTaxIncludedPriceJob(taxRate)
+		priceJob.Process()
 	}
 
-	fmt.Println(result)
 }
--- prices/prices.go
+++ prices/prices.go
@@ -1,9 +1,45 @@
 package prices
 
+import (
+	"fmt"
+
+	"github.com/sgcooper78/go-calculator/conversion"
+	"github.com/sgcooper78/go-calculator/file_manager"
+)
+
 type TaxIncludedPriceJob struct {
 	TaxRate           float64
 	InputPrices       []float64
 	TaxIncludedPrices map[string]float64
+}
+
+func (job *TaxIncludedPriceJob) loadData() {
+	lines, err := file_manager.ReadLines("prices.txt")
+
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+	prices, err := conversion.StringsToFloats(lines)
+
+	if err != nil {
+		fmt.Println("Error converting strings to floats:", err)
+		return
+	}
+
+	job.InputPrices = prices
+}
+
+func (job *TaxIncludedPriceJob) Process() {
+	job.loadData()
+	result := make(map[string]string)
+	for _, price := range job.InputPrices {
+		taxIncludedPrice := price * (1 + job.TaxRate)
+		result[fmt.Sprintf("%.2f", price)] = fmt.Sprintf("%.2f", taxIncludedPrice)
+	}
+
+	fmt.Println(result)
 }
 
 func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
